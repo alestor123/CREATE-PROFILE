b9356b2b4e3a169adc6c1e427c571b5d2efec016
alestor123 <alestoraldous@gmail.com>
2021-08-09 20:50:17 +0530

    added temp app.js

--- App.js
+++ App.js
@@ -1,3 +1,24 @@
-'use strict'
+'use strict';
 const { readFileSync } = require('fs')
 const { render } = require('ejs')
+const socs = require('./socials.json')
+const arrs = ['skills', 'works']
+const mand = ['name', 'description', 'title', 'ghUname']
+module.exports = conf => {
+  if (!(conf && typeof conf === 'object' && checkConf(conf))) throw new Error('Please enter a valid config')
+  return render(readFileSync('./ejs/md.ejs').toString(), { conf, socs, social: filterKeys(conf, Object.keys(socs)) })
+}
+
+function checkConf (conf) {
+  const keys = Object.keys(conf)
+  return keys.length >= 2 && mand.every(i => keys.includes(i) && typeof conf[i] === 'string' && conf[i].length > 0) && commonElement(keys, arrs).every(i => typeof conf[i] === 'object' && conf[i].every(l => typeof l === 'string' || typeof l === 'object'))
+}
+function commonElement (arr1, arr2) {
+  return arr1.filter(n => arr2.indexOf(n) !== -1)
+}
+function filterKeys (main, keys) {
+  return Object.keys(main).filter(key => keys.includes(key)).reduce((obj, key) => {
+    obj[key] = main[key]
+    return obj
+  }, {})
+}
